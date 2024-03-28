ea70ecc2152d839d2a04d4f87cc8c34dc465cc57
marksikaundi <emmanuelmark117@gmail.com>
2024-03-29 01:16:24 +0200

    configured back repo setups with latest pakages

---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+  logo: <strong>Lupleg Docs</strong>,
+  project: {
+    link: "https://github.com/lupleg-dev",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "Nextra Demo - %s",
+    };
+  },
+  footer: {
+    text: "Copyright Lupleg Developer 2024",
+  },
+  
+};
